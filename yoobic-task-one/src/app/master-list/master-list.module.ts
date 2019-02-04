7262f46a3cc5cf6ac2550c80055a40afd5f00a37
Ben A <benjamin.akehurst@foundry.com>
2019-02-04 13:24:37 +0000

    first commit

---
+++ yoobic-task-one/src/app/master-list/master-list.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { MasterListPage } from './master-list.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MasterListPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [MasterListPage]
+})
+export class MasterListPageModule {}
