21193a16d98980d6350fdf21184e9e91b2f0a737
Ben A <benjamin.akehurst@foundry.com>
2019-02-04 14:28:34 +0000

    Call service to get users in home page

--- yoobic-task-one/src/app/home/home.page.ts
+++ yoobic-task-one/src/app/home/home.page.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeopleService } from '../people.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  listOfUsers: any = [];
+
+  constructor(
+    private ps: PeopleService
+  ) { }
+
+  ngOnInit() {
+    this.getUsersList();
+  }
+
+  public getUsersList() {
+    this.ps.getUsersList().subscribe(result => {
+      console.log(result);
+    });
+  }
+
 
 }
