e317bf9a50a81724f4a70a54e89a97c6fb9a1aca
benAkehurst <benakehurst@hotmail.com>
2019-02-07 22:41:57 +0000

    playing with gifs on the detail page

--- yoobic-task-one/src/app/detail/detail.page.ts
+++ yoobic-task-one/src/app/detail/detail.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PeopleService } from '../people.service';
 import { Router } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-detail',
@@ -12,9 +13,14 @@ export class DetailPage implements OnInit {
   chosenUser: any;
   errorOccured: Boolean = false;
   errorMessage: String = '';
+  gifsArr: any = [];
+  randomGif: any;
+  randomGifExists: Boolean = false;
+  showGifIframe: Boolean = false;
 
   constructor(
     private ps: PeopleService,
+    private dataService: DataService,
     private router: Router
   ) {
   }
@@ -25,6 +31,7 @@ export class DetailPage implements OnInit {
 
   public getChosenUser() {
     const chosenUserFromService = this.ps.chosendUser;
+    console.log(chosenUserFromService);
     if (!chosenUserFromService) {
       this.returnToHome();
     } else if (chosenUserFromService) {
@@ -34,13 +41,31 @@ export class DetailPage implements OnInit {
   }
 
   public loadData() {
-    if (!this.chosenUser.location) {
+    if (!this.chosenUser) {
       this.errorOccured = true;
       this.errorMessage = 'Oops, something went wrong. Going Home!';
       this.returnToHome();
     } else {
       console.log(this.chosenUser);
+      this.fetchRandomGif();
     }
+  }
+
+  public fetchRandomGif() {
+    this.dataService.getGifsData().subscribe(result => {
+      this.gifsArr = result.data.children;
+      this.fetchSingleGif();
+    });
+  }
+
+  public fetchSingleGif() {
+    const rand = this.gifsArr[Math.floor(Math.random() * this.gifsArr.length)];
+    this.randomGif = rand.data;
+    this.randomGifExists = true;
+  }
+
+  public seeGif() {
+    this.showGifIframe = true;
   }
 
   public returnToHome() {
